3e86fd9f263de151356574dca1f4696901d75c5b
Shaik Firdos <fidduraza9876@gmail.com>
2024-11-13 17:18:03 +0530

    Added heading and max-width-wrapper components and also made changes in tailwind.config.ts file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
+import { Heading } from "./components/heading"
+import MaxWidthWrapper from "./components/max-width-wrapper"
+
 const Page = () => {
-  return <p>Page</p>
+  return (
+    <>
+      <section className="relative py-24 sm:py-32 bg-brand-25">
+        <MaxWidthWrapper className="text-center">
+          <div className="relative mx-auto text-center flex flex-col items-center gap-10">
+            <div>
+              <Heading>
+                <span>Real-Time SaaS Insights,</span>
+                <br />
+                <span className="relative bg-gradient-to-r from-brand-700 to-brand-800 text-transparent bg-clip-text">
+                  Delivered to Your Discord
+                </span>
+              </Heading>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
+    </>
+  )
 }
 
 export default Page
