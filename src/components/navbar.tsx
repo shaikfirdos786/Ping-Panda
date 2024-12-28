71ac710ea9e022e0b93010ce8f71160f8f04dfea
Shaik Firdos <fidduraza9876@gmail.com>
2024-12-28 22:33:19 +0530

    Added sign-in and sign-up pages using clerk

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,9 +3,10 @@ import MaxWidthWrapper from "./max-width-wrapper"
 import { Button, buttonVariants } from "./ui/button"
 import { SignOutButton } from "@clerk/nextjs"
 import { ArrowRight } from "lucide-react"
+import { currentUser } from "@clerk/nextjs/server"
 
-export const Navbar = () => {
-  const user = false
+export const Navbar = async () => {
+  const user = await currentUser()
   return (
     <nav className="sticky z-[100] h-16 inset-x-0 top-0 w-full border-b border-gray-200 bg-white/80 backdrop-blur-lg transition-all">
       <MaxWidthWrapper>
@@ -55,7 +56,7 @@ export const Navbar = () => {
                 </Link>
 
                 <div className="h-8 w-px bg-gray-200" />
-                
+
                 <Link
                   href="/sign-up"
                   className={buttonVariants({
