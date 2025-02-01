73e4a378c89411ad2c4dfe2d1efcce29376c922a
Shaik Firdos <fidduraza9876@gmail.com>
2025-02-01 22:44:03 +0530

    added dashboard and now working on adding add form

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,9 @@ import { db } from "@/db"
 import { currentUser } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation"
 import { DashboardPageContent } from "./dashboard-page-content"
+import { CreateEventCategoryModal } from "@/components/create-event-category-modal"
+import { Button } from "@/components/ui/button"
+import { PlusIcon } from "lucide-react"
 
 const Page = async () => {
   const auth = await currentUser()
@@ -18,7 +21,14 @@ const Page = async () => {
   }
 
   return (
-    <DashboardPage title="Dashboard">
+    <DashboardPage
+      cta={
+        <CreateEventCategoryModal>
+          <Button><PlusIcon className="size-4 mr-2" /> Add Category</Button>
+        </CreateEventCategoryModal>
+      }
+      title="Dashboard"
+    >
       <DashboardPageContent />
     </DashboardPage>
   )
